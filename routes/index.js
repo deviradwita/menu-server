b5b1ff37a3a4b904924f972a5d19fc4cd9e2369e
devira <devdwita@gmail.com>
2023-03-06 09:08:11 +0700

    finale

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const login = require ('../routes/login')
 const authentication = require('../middleware/authentication')
 const user = require ('../routes/user')
 const history = require('../routes/history')
-const public = require ('../routes/public')
+const publicUser = require ('../routes/public')
 const bookmark = require('./bookmark')
 
 
@@ -15,7 +15,7 @@ const bookmark = require('./bookmark')
 
 router.use(login)
 router.use(register);
-router.use(public)
+router.use(publicUser)
 // router.get('/public/foods', PublicController.fetchFoods)
 // router.get('/public/foods/:id', PublicController.fetchFoodById)
 // router.get('/public/categories', PublicController.fetchCategories)
